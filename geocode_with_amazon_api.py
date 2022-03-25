f3e96d2203d92623db93c41fc91c620c82c79148
abdul-khan-s <101845858+abdul-khan-s@users.noreply.github.com>
2022-03-25 10:54:31 -0400

    Update geocode_with_amazon_api.py

--- geocode_with_amazon_api.py
+++ geocode_with_amazon_api.py
@@ -5,8 +5,10 @@ AWS_SECRET_ACCESS_KEY = ''   ## Add the secret key here that you get from JK
 
 addr_list = ['Boston, MA united states', 'Paris, France', 'Milan, Italy', '20 W 34th St, New York']
 
-client = boto3.client('location') # Calls AWS Location component
-
+client = boto3.client('location',
+		     AWS_ACCESS_KEY_ID = '' , ## Add access key here
+		     AWS_SECRET_ACCESS_KEY = ''   ## Add the secret key here that you get from JK) # Calls AWS Location component
+		     )
 for i in addr_list:
 	Response = client.search_place_index_for_text(
 		IndexName = 'geocoding_test4',  # This is the AWS Location service name that was created to use HERE.COM
