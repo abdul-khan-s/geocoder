d59a0a1ef716e43f37f3f043682afac519adedc2
abdul-khan-s <101845858+abdul-khan-s@users.noreply.github.com>
2022-03-25 10:50:41 -0400

    Add files via upload

---
+++ geocode_with_amazon_api.py
@@ -0,0 +1,23 @@
+import boto3
+
+AWS_ACCESS_KEY_ID = ''  ## Add access key here
+AWS_SECRET_ACCESS_KEY = ''   ## Add the secret key here that you get from JK
+
+addr_list = ['Boston, MA united states', 'Paris, France', 'Milan, Italy', '20 W 34th St, New York']
+
+client = boto3.client('location') # Calls AWS Location component
+
+for i in addr_list:
+	Response = client.search_place_index_for_text(
+		IndexName = 'geocoding_test4',  # This is the AWS Location service name that was created to use HERE.COM
+	###index is created using Amazon Location service
+		Text = i ) #
+
+
+	Lat_Long = Response['Results'][0]['Place']['Geometry']['Point']  # get lat long
+	Country = Response['Results'][0]['Place']['Label'] # get address
+	#print (Response)
+	print (Lat_Long)  
+	print (Country)
+
+	# write the results back.
